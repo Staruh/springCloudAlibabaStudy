b645a2488b95d3e3ac6bf58f86bbe10e15d6ce05
苏 <980884183@qq.com>
2023-06-15 11:32:49 +0800

    提交添加nacos集群

---
+++ borrow-service/src/main/java/com/example/service/client/userClient.java
@@ -0,0 +1,14 @@
+package com.example.service.client;
+
+import com.example.entity.DbUser;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient("userService")
+public interface userClient {
+    @GetMapping("dbUser/{id}")
+    public ResponseEntity<DbUser> queryById(@PathVariable("id") Integer id);
+
+}
